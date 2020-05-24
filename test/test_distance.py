bb999893b506ef52f5117f1e1badc9d24b870c1e
NadeeshaGarusinghe <nadeeshagarusinghe.17@cse.mrt.ac.lk>
2020-05-24 14:13:13 +0530

    add some test files

--- test/test_distance.py
+++ test/test_distance.py
@@ -10,14 +10,14 @@ class FlaskTestClass(unittest.TestCase):
             '/distance', query_string=dict(media='bus',origin='colombo', destination='matara'))
         self.assertEqual(response.status_code, 200)
 
-    def test_distance_request_fromBUs(self):
+    def test_distance_request_fromTrain(self):
         tester = app.test_client(self)
         response = tester.get(
             '/distance', query_string=dict(media='train',origin='colomboFort', destination='matara'))
         self.assertEqual(response.status_code, 200)
 
     # ensure response for /distance is  correct
-    def test_response(self):
+    def test_responseBUs(self):
         tester = app.test_client(self)
         response = tester.get(
             '/distance', query_string=dict(media='bus',origin='colombo', destination='matara'))
@@ -25,28 +25,35 @@ class FlaskTestClass(unittest.TestCase):
             b'From: colombo To: matara the distance from bus is 160.4 km and run time is 04h 45min (a 20 minute driving break is also included)' in response.data)
 
 # ensure response for /distance is  correct
-    def test_response(self):
+    def test_responseTrain(self):
         tester = app.test_client(self)
         response = tester.get(
-            '/distance', query_string=dict(media='train',origin='colomboFort', destination='gampaha'))
+            '/distance', query_string=dict(media='train',origin='gampaha', destination='colombofort'))
         self.assertTrue(
-            b'From: colomboFort To: Gampaha the distance from train is 28.4km on the track 1' in response.data)
+            b'From: gampaha  To: colombofort  the distance from train is 28.4km   on the track  1' in response.data)
 
     # ensure response for empty inputs
     def test_empty_inputs(self):
         tester = app.test_client(self)
         response = tester.get(
-            '/distance', query_string=dict(destination='matara'))
+            '/distance', query_string=dict(media='bus',destination='matara'))
         self.assertEqual(response.status_code, 400)
 
     # ensure respose is correct for incorrect inputs
-    def test_incorrect_respose(self):
+    def test_incorrect_resposeBUs(self):
         tester = app.test_client(self)
         response = tester.get(
-            '/distance', query_string=dict(origin='aaaaa', destination='bbbbb'))
+            '/distance', query_string=dict(media = 'bus',origin='aaaaa', destination='bbbbb'))
         self.assertTrue(
             b'sorry!please enter a valid origin and destination:('in response.data)
 
+    # ensure respose is correct for incorrect inputs
+    def test_incorrect_resposeTrain(self):
+        tester = app.test_client(self)
+        response = tester.get(
+            '/distance', query_string=dict(media = 'train',origin='aaaaa', destination='bbbbb'))
+        self.assertTrue(
+            b'sorry!please enter a valid origin and destination:('in response.data)
 
 if __name__ == '__main__':
     unittest.main()
